76739b15af00392229372f88757664be60e7c038
StevenX5 <101534728+StevenX5@users.noreply.github.com>
2022-04-02 21:20:31 +0800

    Add files via upload

---
+++ 基础篇/以太坊的安全/智能合约的安全/智能合约缺陷分类和测试用例/contracts/integer_overflow_mul_fixed.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity >=0.4.16;
+
+contract IntegerOverflowMul {
+    uint public count = 2;
+
+    function run(uint256 input) public {
+        count = mul(count, input);
+    }
+
+    //from SafeMath
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+      // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+      // benefit is lost if 'b' is also tested.
+      // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+      if (a == 0) {
+        return 0;
+      }
+
+      uint256 c = a * b;
+      require(c / a == b);
+
+      return c;
+    }
+}
